9feebba2b1b04ce20bf976fb5d51305c77d75dab
Thanthai Jitprathum <thanthai.j@odds.team>
2025-02-13 18:54:05 +0700

    fix style of celebrate with us

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -15,17 +15,24 @@ export default function Home() {
   return (
     <div>
       <Carousel />
-      <div className="text-center text-primary m-8">
-        <p className="text-lg sm:text-xl md:text-3xl font-bold font-sans">
+      <div className="text-center text-primary my-8 px-6 sm:px-12 md:px-16">
+        <p className="text-lg sm:text-xl md:text-3xl font-bold font-sans tracking-wide text-gray-700">
           Let’s come to
         </p>
-        <h1 className="text-lg sm:text-xl md:text-3xl font-extrabold mt-2 font-sans">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold mt-2 text-primary leading-tight">
           Celebrate with us
         </h1>
-        <p className="text-xl sm:text-2xl mt-4">15 - 04 - 2026</p>
-        <p className="text-lg sm:text-xl mt-2">(สถานที่จัดงาน)</p>
-        <CountdownTimer />
+        <p className="text-xl sm:text-2xl mt-4 text-gray-800">15 - 04 - 2026</p>
+        <p className="text-lg sm:text-xl mt-2 text-gray-500">(สถานที่จัดงาน)</p>
+
+        <div className="flex justify-center items-center">
+          <div className="relative">
+            <CountdownTimer />
+            <div className="absolute top-0 left-0 right-0 bottom-0 opacity-60 rounded-lg"></div>
+          </div>
+        </div>
       </div>
+
       <WeddingCard />
       <OurLoveStory />
       <Gallery />
