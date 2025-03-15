fbba7c26333d424217bcf719f79e6ae0708a48e2
Thanthai Jitprathum <thanthai.j@odds.team>
2025-03-15 20:22:42 +0700

    change our wedding text color

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     <nav className="sticky top-0 left-0 right-0 bg-white shadow-md z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex-shrink-0 text-xl font-semibold">Our Wedding</div>
+          <div className="flex-shrink-0 text-xl font-semibold text-gray-700">Our Wedding</div>
         </div>
       </div>
     </nav>
